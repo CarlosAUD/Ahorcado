525d85d9a01ea6e40b016f35db74748f45808b72
AnthonyHaro <150816518+AnthonyHaro@users.noreply.github.com>
2023-11-30 22:35:22 -0500

    creacion del principio de codigo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> banc = new ArrayList<>();
+        Scanner letra = new Scanner(System.in);
+        banc.add("computadora");
+        banc.add("caballo");
+        banc.add("computadora");
+        banc.add("gato");
+        int a = (int) ((Math.random() * 4) + 1);
+        int error = 0, nume;
+        boolean confi = false;
+        String letras;
+        String palabra = banc.get(a);
+        String copia = palabra;
+        StringBuilder copiaModificada = new StringBuilder();
+        for (int i = 0; i < copia.length(); i++) {
+            copiaModificada.append('*');
+        }
+        copia = copiaModificada.toString();
+        while (error != 6 && !confi) {
+            System.out.println("----EL AHORCADO----");
+            System.out.print("Escribe una letra: ");
+            letras = letra.nextLine();
+            int p = palabra.indexOf(letras);
+            if (p != -1) {
+                while (p != -1) {
+                    copia = copia.substring(0, p) + letras + copia.substring(p + 1);
+                    p = palabra.indexOf(letras, p + 1);
+                }
+                System.out.println("Errores: " + error);
+                if (copia.equals(palabra)) {
+                    System.out.println("¡Ganaste!");
+                    confi = true;
+                }
+            } else {
+                System.out.println("Fallaste.");
+                error += 1;
+                mostrarAhorcado(error);
+                System.out.println("Errores: " + error);
+            }
+            System.out.println(copia);
+
+        }
     }
 }
